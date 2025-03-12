a188836c6e4cda8db3f448a108b36837899bba1f
Jose Climaco <jclimaco@insurads.com>
2025-03-12 12:01:24 +0000

    Fast Fetch InsurAds - Functionalities Validation

--- ads/_config.js
+++ ads/_config.js
@@ -693,17 +693,17 @@ const adConfig = jsonConfiguration({
     renderStartImplemented: true,
   },
 
-  'insurads': {
-    prefetch: 'https://securepubads.g.doubleclick.net/tag/js/gpt.js',
-    preconnect: [
-      'https://tpc.googlesyndication.com',
-      'https://cdn.insurads.com',
-      'https://services.insurads.com',
-      'https://messaging.insurads.com',
-    ],
-    renderStartImplemented: true,
-    consentHandlingOverride: true,
-  },
+  // 'insurads': {
+  //   prefetch: 'https://securepubads.g.doubleclick.net/tag/js/gpt.js',
+  //   preconnect: [
+  //     'https://tpc.googlesyndication.com',
+  //     'https://cdn.insurads.com',
+  //     'https://services.insurads.com',
+  //     'https://messaging.insurads.com',
+  //   ],
+  //   renderStartImplemented: true,
+  //   consentHandlingOverride: true,
+  // },
 
   'invibes': {
     prefetch: 'https://k.r66net.com/GetAmpLink',
